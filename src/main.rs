0bd68bdd56ab6e6ca7f4fdaafb5e3e55213a2037
Seb Ospina <kraige@gmail.com>
2026-06-25 20:46:03 +0200

    Init feathers menu

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use bevy::camera_controller::free_camera::FreeCameraPlugin;
+use bevy::feathers::{
+    FeathersPlugins,
+    controls::*,
+    dark_theme::create_dark_theme,
+    theme::{ThemedText, UiTheme},
+};
+use bevy::input_focus::tab_navigation::TabGroup;
 use bevy::prelude::*;
+use bevy::ui_widgets::Activate;
 use bevy_skein::SkeinPlugin;
 use clap::Parser;
 use swarmy_bevy::MapScene;
@@ -13,11 +21,17 @@ pub struct MapPlugin;
 
 impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
+        app.add_systems(Startup, MapPlugin::scene.spawn());
         app.add_systems(Startup, setup_light_and_gizmo_control_text);
         app.add_systems(Startup, load_t3_height_map);
         app.add_systems(Update, load_cache_objects);
         app.add_systems(Update, load_t3_terrain);
         app.add_systems(Update, update_gizmo_config);
+    }
+}
+impl MapPlugin {
+    fn scene() -> impl SceneList {
+        bsn_list![init_feathers()]
     }
 }
 
@@ -37,9 +51,11 @@ fn main() {
         })
         .init_gizmo_group::<MyRoundGizmos>()
         .add_plugins(DefaultPlugins)
+        .add_plugins(FeathersPlugins)
         .add_plugins(SkeinPlugin::default())
         .add_plugins(FreeCameraPlugin)
         .add_plugins(MapPlugin)
+        .insert_resource(UiTheme(create_dark_theme()))
         .add_systems(PreStartup, load_gltf)
         .run();
 }
@@ -65,7 +81,7 @@ fn setup_light_and_gizmo_control_text(mut commands: Commands) {
         ),
         Node {
             position_type: PositionType::Absolute,
-            top: px(12),
+            bottom: px(12),
             left: px(12),
             ..default()
         },
@@ -74,13 +90,6 @@ fn setup_light_and_gizmo_control_text(mut commands: Commands) {
             ..default()
         },
     ));
-}
-
-#[derive(Component, Default, Reflect, Debug)]
-#[reflect(Component, Default)]
-#[type_path = "api"]
-struct Character {
-    name: String,
 }
 
 fn load_gltf(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -98,4 +107,103 @@ fn load_gltf(mut commands: Commands, asset_server: Res<AssetServer>) {
     ));*/
     let gltf = asset_server.load("swarmy-objects.gltf");
     commands.insert_resource(MapScene(gltf));
+}
+
+fn init_feathers() -> impl Scene {
+    bsn! {
+        Node {
+            width: percent(10),
+            height: percent(10),
+            align_items: AlignItems::Start,
+            justify_content: JustifyContent::Start,
+            display: Display::Flex,
+            flex_direction: FlexDirection::Row,
+            column_gap: px(8),
+        }
+        TabGroup
+        Children[
+            feather_column_1(),
+        ]
+    }
+}
+fn feather_column_1() -> impl Scene {
+    bsn! {
+        Node {
+            display: Display::Flex,
+            flex_direction: FlexDirection::Column,
+            justify_content: JustifyContent::Start,
+            padding: px(8),
+            row_gap: px(8),
+            width: percent(30),
+            min_width: px(200),
+        }
+        Children [
+            (
+                Node {
+                    display: Display::Flex,
+                    flex_direction: FlexDirection::Row,
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::Start,
+                    column_gap: px(8),
+                }
+                Children [
+                    main_feather_menu(),
+                ]
+            )
+        ]
+    }
+}
+
+fn main_feather_menu() -> impl Scene {
+    bsn! {
+        (
+            @FeathersMenu
+            Children [
+                (
+                    @FeathersMenuButton {
+                        @caption: bsn! { Text("View") ThemedText }
+                    }
+                    AccessibleLabel("View Menu")
+                    Node {
+                        flex_grow: 1.0,
+                    }
+                ),
+                (
+                    @FeathersMenuPopup
+                    Children [
+                        (
+                            @FeathersCheckbox {
+                                @caption: bsn! { Text("Enable ObjectDoodas") ThemedText }
+                            }
+                            on(|_: On<Activate>| {
+                                info!("Enabling ObjectDoodas");
+                            })
+                        ),
+                        (
+                            @FeathersCheckbox {
+                                @caption: bsn! { Text("Enable ObjectUnits") ThemedText }
+                            }
+                            on(|_: On<Activate>| {
+                                info!("Enabling ObjectUnits");
+                            })
+                        ),
+                        (
+                            @FeathersCheckbox {
+                                @caption: bsn! { Text("Enable ObjectPoints") ThemedText }
+                            }
+                            on(|_: On<Activate>| {
+                                info!("Enabling ObjectPoints");
+                            })
+                        ),
+                        @FeathersMenuDivider,
+                        (
+                            @FeathersMenuItem {
+                                @caption: bsn! { Text("Second section") ThemedText }
+                            }
+                        )
+                    ]
+                )
+            ]
+        )
+    }
 }
